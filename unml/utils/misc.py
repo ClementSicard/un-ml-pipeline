382b41a6796d266d66fd2a17b330735b2aa85e0a
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-14 19:28:03 +0200

    misc: added docstrings

--- unml/utils/misc.py
+++ unml/utils/misc.py
@@ -1,10 +1,62 @@
+"""
+This module contains miscellaneous functions and classes.
+"""
+
 from loguru import logger
 
 from .consts import DEFAULT_LOGGER_LEVEL, LOGGER_LEVELS
 
 
 def log(message: str, verbose: bool, level: str = DEFAULT_LOGGER_LEVEL):
+    """
+    Log function to print messages to the console.
+
+    Parameters
+    ----------
+    `message` : `str`
+        The message to be print
+    `verbose` : `bool`
+        If verbose is `False`, the message won't be printed
+    `level` : `str`, optional
+        The `loguru` log level, by default DEFAULT_LOGGER_LEVEL
+    """
+
     assert level in LOGGER_LEVELS, f"Invalid level: {level}"
 
-    if verbose:
-        logger.debug(message)
+    if not verbose:
+        return
+
+    match level:
+        case "debug":
+            logFunc = logger.debug
+        case "info":
+            logFunc = logger.info
+        case "warning":
+            logFunc = logger.warning
+        case "error":
+            logFunc = logger.error
+        case "success":
+            logFunc = logger.success
+        case _:
+            logFunc = logger.debug
+
+    logFunc(message)
+
+
+def isCorrectURL(url: str) -> bool:
+    """
+    Checks if the given URL is valid.
+
+    Parameters
+    ----------
+    `url` : `str`
+        The URL to be checked
+
+    Returns
+    -------
+    `bool`
+        `True` if the URL is valid, `False` otherwise
+    """
+    import validators
+
+    return validators.url(url)
