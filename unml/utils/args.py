a4a23c22eef530ee15e1878125e7fe11964de81c
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-20 20:01:00 +0200

    args: added URLs from files

--- unml/utils/args.py
+++ unml/utils/args.py
@@ -1,55 +1,112 @@
 """
 This module contains helpers to parse command line arguments.
-
-Returns
--------
-`_type_`
-    _description_
 """
 
 from argparse import ArgumentParser
-from typing import Any, Dict
+from typing import Any, Dict, List
 
-from unml.utils.misc import log
+from loguru import logger
+
+from unml.utils.misc import isCorrectURL, log
 
 
-def parseArgs() -> Dict[str, Any]:
+class ArgUtils:
     """
-    Parses the command line arguments.
-
-    Returns
-    -------
-    `Dict[str, Any]`
-        The parsed arguments as a dictionary
+    Utility class for CLI arguments related tasks.
     """
-    parser = ArgumentParser()
 
-    parser.add_argument("-u", "--url", type=str, required=False)
-    parser.add_argument("-f", "--file", type=str, required=False)
+    @staticmethod
+    def parseArgs() -> Dict[str, Any]:
+        """
+        Parses the command line arguments.
 
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        default=False,
-    )
+        Returns
+        -------
+        `Dict[str, Any]`
+            The parsed arguments as a dictionary
+        """
+        parser = ArgumentParser()
 
-    parsedArgs = vars(parser.parse_args())
+        parser.add_argument("-u", "--url", type=str, required=False)
+        parser.add_argument("-f", "--file", type=str, required=False)
 
-    if not parsedArgs.get("url") and not parsedArgs.get("file"):
-        log(
-            "You need to specify either a URL or a file. Please try again",
-            level="error",
-            verbose=True,
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+            default=False,
         )
-        exit()
 
-    if parsedArgs.get("url") and parsedArgs.get("file"):
-        log(
-            "You specified both a URL and a file. The file will be discarded",
-            level="warning",
-            verbose=True,
-        )
-        del parsedArgs["file"]
+        parsedArgs = vars(parser.parse_args())
 
-    return parsedArgs
+        if not parsedArgs.get("url") and not parsedArgs.get("file"):
+            log(
+                "You need to specify either a URL or a file. Please try again",
+                level="error",
+                verbose=True,
+            )
+            exit()
+
+        if parsedArgs.get("url") and parsedArgs.get("file"):
+            log(
+                "You specified both a URL and a file. The URL will be discarded",
+                level="warning",
+                verbose=True,
+            )
+            del parsedArgs["url"]
+
+        log(f"Arguments: {parsedArgs}", verbose=parsedArgs["verbose"])
+
+        return parsedArgs
+
+    @staticmethod
+    def getURLsFromArgs(args: Dict[str, Any]) -> List[str]:
+        """
+        Extract a singleton (URL case) or list of paths/urls (file case) depending
+        on the CLI arguments.
+
+        Parameters
+        ----------
+        `args` : `Dict[str, Any]`
+            The CLI arguments
+
+        Returns
+        -------
+        `List[str]`
+            The list of paths/URLs
+        """
+        results = []
+
+        # Case 1: URL CLI arg is specified
+        if args.get("url"):
+            url: str = args["url"]
+
+            if isCorrectURL(url=url):
+                results.append(url)
+            else:
+                logger.error(f"'{args.get('url')}' is not a valid URL")
+                exit()
+
+        # Case 2: file CLI arg is specified
+        elif args.get("file"):
+            path: str = args["file"]
+            try:
+                with open(path, "r") as f:
+                    for line in f.readlines():
+                        if isCorrectURL(line):
+                            results.append(line)
+                        else:
+                            logger.warning(f"'{args.get('url')}' is not a valid URL")
+
+                if not results:
+                    logger.error(f"No valid URLs found in {path}.")
+                    exit()
+
+            except FileNotFoundError:
+                logger.error(f"File '{path}' not found")
+                exit()
+            except Exception as err:
+                logger.error(f"Error while reading file '{path}': {err}")
+                exit()
+
+        return results
