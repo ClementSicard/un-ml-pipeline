f2862fa9b29b8dd4dd84dff19aebf84e66c210d5
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-22 17:47:13 +0200

    logging: adds penultimate function

--- unml/utils/misc.py
+++ unml/utils/misc.py
@@ -1,13 +1,57 @@
 """
 This module contains miscellaneous functions and classes.
 """
+import inspect
+import os
+import sys
 
 from loguru import logger
 from transformers.utils import logging as hfLogging
 
+from unml.utils.consts.io import IOConsts
 from unml.utils.consts.logger import LoggerConsts
 
 hfLogging.set_verbosity(40)
+
+
+def getPenultimateFunctionName() -> str:
+    """
+    Get the penultimate function name in the stack.
+
+    Returns
+    -------
+    `str`
+        The penultimate function name in the stack
+    """
+    # Get the current stack
+    stack = inspect.stack()
+
+    # Get the penultimate frame if exists
+    if len(stack) >= 3:
+        penultimate_frame = stack[2]
+        fileName = (
+            os.path.relpath(
+                penultimate_frame.filename,
+                IOConsts.PROJECT_ROOT,
+            )
+            .replace("/", ".")
+            .replace(".py", "")
+        )
+        return f"{fileName}:{penultimate_frame.function}:{penultimate_frame.lineno}"
+    else:
+        return "N/A"
+
+
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level>"
+    + " | <cyan>{extra[penultimate]}</cyan> | <level>{message}</level>",
+    level="DEBUG",
+    colorize=True,
+    diagnose=False,
+    backtrace=False,
+)
 
 
 def log(
@@ -31,19 +75,21 @@ def log(
     if not verbose:
         return
 
+    bindedLogger = logger.bind(penultimate=getPenultimateFunctionName())
+
     match level:
         case "debug":
-            logFunc = logger.debug
+            logFunc = bindedLogger.debug
         case "info":
-            logFunc = logger.info
+            logFunc = bindedLogger.info
         case "warning":
-            logFunc = logger.warning
+            logFunc = bindedLogger.warning
         case "error":
-            logFunc = logger.error
+            logFunc = bindedLogger.error
         case "success":
-            logFunc = logger.success
+            logFunc = bindedLogger.success
         case _:
-            logFunc = logger.debug
+            logFunc = bindedLogger.debug
 
     logFunc(message)
 
