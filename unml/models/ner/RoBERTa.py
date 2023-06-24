bdd81b8ca427c40d5ffad4777d16d32ff00f1c02
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-24 18:05:14 +0200

    RoBERTa: moved chunked recognition to model instead of module

--- unml/models/ner/RoBERTa.py
+++ unml/models/ner/RoBERTa.py
@@ -1,11 +1,15 @@
 from typing import Any, Dict, List
 
+from tqdm import tqdm
+
 from unml.models.model import Model
+from unml.utils.text import TextUtils
 
 
 class RoBERTa(Model):
     """
     Class for RoBERTa (Liu et al., 2019) model
+    https://arxiv.org/abs/1907.11692
     """
 
     MODEL_NAME = "Jean-Baptiste/roberta-large-ner-english"
@@ -17,6 +21,40 @@ class RoBERTa(Model):
         """
         See doc for `NamedEntityRecognizer` class
         """
-        result: List[Dict[str, Any]] = self.model(text)
+        return self.recognizeFromChunked(text=text)
 
-        return result
+    def recognizeFromChunked(self, text: str) -> List[Dict[str, Any]]:
+        """
+        Recognize named entities from text, chunking the text into smaller
+        pieces to avoid hitting the max sequence length limit.
+
+        Parameters
+        ----------
+        `text` : `str`
+            Text to recognize named entities from.
+
+        Returns
+        -------
+        `List[Dict[str, Any]]`
+            List of named entities recognized from the text.
+        """
+        tokenizer = self.model.tokenizer
+
+        tokens = tokenizer.tokenize(text)
+
+        chunkedTokens = TextUtils.chunkTokens(
+            tokens=tokens,
+            tokenizer=tokenizer,
+        )
+
+        results = []
+        for chunk in tqdm(chunkedTokens):
+            chunkResults = self.model(chunk)
+            results.extend(chunkResults)
+
+        # Type casting for JSON serialization and cleaning
+        for result in results:
+            result["score"] = float(f'{result["score"]:.3f}')
+            result["word"] = result["word"].strip()
+
+        return results
