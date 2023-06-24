84a9c4eb63fa8132f906648a4e6003e6b8a0fdc5
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-24 18:17:57 +0200

    text.py: remove deletion of sequences of 4+ dots

--- unml/utils/text.py
+++ unml/utils/text.py
@@ -89,9 +89,6 @@ class TextUtils:
         # Harmonize spacing
         text = re.sub(r"\s+", " ", text)
 
-        # Remove sequences of long dots
-        text = re.sub(r"\.{4,}", "", text)
-
         # Remove spaces before punctuation
         text = text.replace(" .", ".")
 
