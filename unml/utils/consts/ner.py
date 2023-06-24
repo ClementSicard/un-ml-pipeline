3dec5f924abe4716cb8724572955db72c59a1623
Clément Sicard <c.clement.sicard@northvolt.com>
2023-06-24 18:12:57 +0200

    ner: adds spaCy as default NER model

--- unml/utils/consts/ner.py
+++ unml/utils/consts/ner.py
@@ -3,11 +3,12 @@ class NERConsts:
     Named Entity Recognition constants
     """
 
-    MODELS = {"RoBERTa", "FLERT"}
+    MODELS = {"RoBERTa", "FLERT", "spaCyNER"}
 
-    DEFAULT_NER_MODEL = "RoBERTa"
+    DEFAULT_NER_MODEL = "spacy"
 
     ARGS_MAP = {
         "roberta": "RoBERTa",
         "flert": "FLERT",
+        "spacy": "spaCyNER",
     }
