954bcc7965299cafc655c062dbe37d0d3285c804
Clément Sicard <c.clement.sicard@northvolt.com>
2023-07-01 17:23:57 +0200

    document: adds other fields from API to document base model

--- unml/utils/types/document.py
+++ unml/utils/types/document.py
@@ -2,17 +2,7 @@ from typing import Any, Dict, List, Optional
 
 from pydantic import BaseModel
 
-
-class QueryDocument(BaseModel):  # type: ignore
-    """
-    This class models a potentially incomplete document.
-    """
-
-    recordId: str
-    title: str
-    url: Optional[str] = None
-    summary: Optional[str] = None
-    namedEntities: Optional[Dict[str, Dict[str, int] | List[Dict[str, Any]]]] = None
+from unml.utils.text import TextUtils
 
 
 class Document(BaseModel):  # type: ignore
@@ -23,21 +13,105 @@ class Document(BaseModel):  # type: ignore
     recordId: str
     title: str
     url: str
+    altTitle: Optional[str] = None
+    location: Optional[str] = None
+    symbol: Optional[str] = None
+    publicationDate: Optional[str] = None
+    relatedDocuments: Optional[List["Document"]] = None
+    # TODO: Add the bodies individually to the graph database
+    unBodies: Optional[List[str]] = None
+
     summary: Optional[str] = None
     namedEntities: Optional[Dict[str, Dict[str, int] | List[Dict[str, Any]]]] = None
 
-    def toParams(self) -> Dict[str, Any]:
+    def toGraphDBObject(self) -> str:
         """
-        Convert the document to a dictionary of parameters.
+        Convert the document to a Cypher query.
 
         Returns
         -------
-        `Dict[str, Any]`
-            The dictionary of parameters
+        `str`
+            The query
+
+
+        Example
+        --------
+        ```python
+        >>> doc = Document(
+        ...     recordId="A/RES/75/1",
+        ...     title="The situation in the Middle East",
+        ...     altTitle="The situation in the Middle East",
+        ...     location="New York",
+        ...     symbol="A/RES/75/1",
+        ...     publicationDate="2020-09-15",
+        ...     summary="The situation in the Middle East",
+        ...     url="https://undocs.org/A/RES/75/1",
+        ... )
+        >>> doc.toGraphDBObject()
+        'p:Document {
+            id: "A/RES/75/1",
+            title: "The situation in the Middle East",
+            altTitle: "The situation in the Middle East",
+            location: "New York",
+            symbol: "A/RES/75/1",
+            publicationDate: "2020-09-15",
+            summary: "The situation in the Middle East",
+            url: "https://undocs.org/A/RES/75/1"
+        }'
+        ```
         """
-        return {
-            "id": self.recordId,
-            "title": self.title,
-            "summary": self.summary,
-            "url": self.url,
-        }
+        return """d:Document
+            {{
+                id: "{recordId}",
+                title: "{title}",
+                altTitle: "{altTitle}",
+                location: "{location}",
+                symbol: "{symbol}",
+                publicationDate: "{publicationDate}",
+                summary: "{summary}",
+                url: "{url}"
+            }}
+        """.format(
+            recordId=TextUtils.replaceIfNotNull(self.recordId, '"', '\\"'),
+            title=TextUtils.replaceIfNotNull(self.title, '"', '\\"'),
+            altTitle=TextUtils.replaceIfNotNull(self.altTitle, '"', '\\"'),
+            location=TextUtils.replaceIfNotNull(self.location, '"', '\\"'),
+            symbol=TextUtils.replaceIfNotNull(self.symbol, '"', '\\"'),
+            publicationDate=TextUtils.replaceIfNotNull(
+                self.publicationDate, '"', '\\"'
+            ),
+            summary=TextUtils.replaceIfNotNull(self.summary, '"', '\\"'),
+            url=TextUtils.replaceIfNotNull(self.url, '"', '\\"'),
+        )
+
+    @staticmethod
+    def fromLibraryAPIResponse(response: Dict[str, Any]) -> "Document":
+        """
+        Parse the information from the response of the UNDL API.
+
+        Parameters
+        ----------
+        `response` : `Dict[str, Any]`
+            Response of the UNDL API
+
+        Returns
+        -------
+        `Document`
+            The parsed document.
+        """
+        downloads = response.get("downloads")
+
+        if downloads is None or downloads.get("English") is None:
+            raise ValueError("No downloads found in the response")
+        else:
+            return Document(
+                recordId=response.get("id"),
+                title=response.get("title"),
+                altTitle=response.get("altTitle"),
+                location=response.get("location"),
+                symbol=response.get("symbol"),
+                publicationDate=response.get("publicationDate"),
+                url=downloads.get("English"),
+                relatedDocuments=response.get("relatedDocuments"),
+                unBodies=response.get("unBodies"),
+            )
