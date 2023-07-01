b5e69dcf5777572a96ee77a0a7a38a3a470cfa21
Clément Sicard <c.clement.sicard@northvolt.com>
2023-07-01 17:37:14 +0200

    api: checks connection at API start + fixes

--- unml/api.py
+++ unml/api.py
@@ -1,18 +1,25 @@
+import json
 from typing import List
 
 from fastapi import FastAPI, HTTPException
 from undl.client import UNDLClient
 
+from unml.graphdb.graphdb import GraphDB
 from unml.main import runPipelines
 from unml.utils.consts.api import APIConsts
 from unml.utils.misc import log
-from unml.utils.types.document import Document, QueryDocument
+from unml.utils.types.document import Document
 from unml.utils.types.json import JSON
+from unml.utils.types.record import Record
+
+clientUNDL = UNDLClient(verbose=True)
+graphDB = GraphDB()
 
 app = FastAPI(
     debug=True,
     title="UNML API",
     description="API for the machine learning pipeline of the UNML project",
+    on_startup=[graphDB.checkConnection],
 )
 
 
@@ -30,45 +37,58 @@ def readRoot() -> JSON:
 
 
 @app.post("/run")  # type: ignore
-def run(queryDocs: List[QueryDocument]) -> List[JSON]:
+def run(records: List[Record]) -> List[JSON]:
     """
-    Post a list of documents to run the pipeline on.
+    Post a list of record IDs to run the pipeline on the documents
+    corresponding to them.
+
+    The pipeline is run with the default arguments, defined in `APIConsts` in
+    `unml/utils/consts/api.py`.
 
     Parameters
     ----------
-    `docs` : `List[QueryDocument]`
-        The list of documents to run the pipeline on
+    `docs` : `List[Record]`
+        The list of record IDs
 
     Returns
     -------
     `List[JSON]`
         The list of documents with the pipeline results
     """
-    for doc in queryDocs:
-        if doc.url is None:
-            client = UNDLClient(verbose=True)
-            queryResult = client.queryById(
-                recordId=doc.recordId,
-                oldURL=True,
-                outputFormat="marcxml",
-            )
-            try:
-                url = queryResult[0]["downloads"]["English"]
-                doc.url = url
+    parsedDocs = []
 
-            except KeyError as e:
-                log(f"Error: {e}", level="error", verbose=True)
-                # Error: no URL or file specified
+    for record in records:
+        # 1. Query the API with the record ID to get the document info
+        queryResult = clientUNDL.queryById(
+            recordId=record.recordId,
+            oldURL=True,
+            outputFormat="marcxml",
+        )
+        log(
+            json.dumps(queryResult, indent=4, ensure_ascii=False),
+            verbose=True,
+            level="debug",
+        )
+        try:
+            # 2. Create a document object from the API response
+            doc = Document.fromLibraryAPIResponse(response=queryResult[0])
+            parsedDocs.append(doc)
+
+        except KeyError as e:
+            log(f"Error: {e}", level="error", verbose=True)
+
+            # Error: no URL or file specified
+            if "English" not in e.args[0]:
                 raise HTTPException(
-                    status_code=404, detail=f"No URL found for id '{doc.recordId}'"
+                    status_code=404,
+                    detail=f"No URL found for id '{record.recordId}'",
                 )
-            except Exception as e:
-                log(f"Error: {e}", level="error", verbose=True)
-                # Error: no URL or file specified
+            else:
                 raise HTTPException(status_code=500, detail="Internal server error")
 
-            log(f"Query result: {queryResult}", verbose=True)
+        except Exception as e:
+            log(f"Error: {e}", level="error", verbose=True)
+            # Error: no URL or file specified
+            raise HTTPException(status_code=500, detail="Internal server error")
 
-    docs: List[Document] = [Document(**doc.dict()) for doc in queryDocs]
-
-    return runPipelines(docs=docs, args=APIConsts.DEFAULT_PIPELINE_ARGS)
+    return runPipelines(docs=parsedDocs, args=APIConsts.DEFAULT_PIPELINE_ARGS)
